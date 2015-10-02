51460beca7e2feaa0833e5cb509acf13296b6a00
shmuelko <shmuel777@gmail.com>
2015-10-02 18:08:53 +0200

    make save and load

--- popup.js
+++ popup.js
@@ -1,18 +1,27 @@
 $(function(){
 	$('.save-btn').click(save);
 	$('.load-btn').click(load);
-	alert(0);
-	debugger;
+	//alert(0);
+	//debugger;
 	console.log('Hello World');
 });
 
 var save = function() {
-	console.log('Hello');
-	localStorage.scrollY = window.pageYOffset;
+	
+	chrome.tabs.executeScript({
+		code: 'console.log(\'Hello\');' +
+				'localStorage.scrollY = window.pageYOffset;'
+	});
+	
+	
 	
 };
  
  var load = function() { 
-	window.scroll(0, localStorage.scrollY);
-	console.log('Boo');
+	 chrome.tabs.executeScript({
+		code: 'window.scroll(0, localStorage.scrollY);' +
+				'console.log(\'Boo\');'
+	});
+	
+	
 };

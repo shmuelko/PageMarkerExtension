bcf95b2ee58586240dbd8a0ff5f5ee964614f5ea
shmuelko <shmuel777@gmail.com>
2015-10-04 11:29:56 +0200

    Adding strips (V 0.1)
    showing on the page where is the mark point on.

--- popup.js
+++ popup.js
@@ -10,7 +10,24 @@ var save = function() {
 	
 	chrome.tabs.executeScript({
 		code: 'console.log(\'Hello\');' +
-			'localStorage.setItem(window.location.href,window.pageYOffset);'
+			//'$(\'.page-marker\').remove()'+
+			'localStorage.setItem(window.location.href,window.pageYOffset);'+
+			
+			'var div = document.createElement(\"div\");'+
+			'div.style.width = (window.innerWidth * 2/100) +\"px\";'+
+			'div.style.height = (window.innerHeight * 2/100) +\"px\";'+
+			'div.style.background = \"green\";'+	
+			
+			'div.style.position = \"fixed\";'+
+			'div.style.marginLeft = (window.innerWidth - 20 )+\"px\";'+ 
+			'div.style.top = (10 + (window.pageYOffset/ window.innerHeight * 100)) +\"px\";'+
+			'div.className = \"page-marker\";'+ 
+			
+			'console.log(\'window.innerHeight: \'+window.innerHeight);' +
+			'console.log(\'window.pageYOffset: \'+window.pageYOffset);' +
+			'console.log(\'mark place: \'+window.pageYOffset / window.innerHeight * 100);' +
+
+			'document.body.appendChild(div);'
 				
 	});
 	
